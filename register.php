9843895b5d69650264be64c936003dc18ec08bec
Zattack123 <zdilliha@gmail.com>
2021-03-16 22:11:51 -0500

    adding login/finish register

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 //Include the database config file
-require_once "/includes/dbh.inc.php";
+require_once "includes/dbh.inc.php";
 
 //define variables and init them to empty
 $username = $password = $confirm_password ="";
@@ -11,14 +11,14 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
   //validate username
   if(empty(trim($_POST["username"]))) {
-    $username_err = "Please enter a username"
+    $username_err = "Please enter a username";
   }else{
     //prepare a select statment
     $sql = "SELECT uid FROM users WHERE username = ?";
 
     if($stmt = mysqli_prepare($conn,$sql)) {
       //bind variables to the prepared statement as parameters
-      mysqli_bind_param($stmt, "s", $param_username);
+      mysqli_stmt_bind_param($stmt, "s", $param_username);
       //set parameters
       $param_username = trim($_POST["username"]);
       //attempt to execute the prepared statement
@@ -27,9 +27,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         mysqli_stmt_store_result($stmt);
 
         if(mysqli_stmt_num_rows($stmt)==1){
-          $username_err = "This USername is already taken, please try another";
+          $username_err = "This Username is already taken, please try another";
         }else{
-          $username = trim($_PSOT["username"]);
+          $username = trim($_POST["username"]);
         }
       }else{
         echo "Something went wrong";
@@ -38,8 +38,8 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
   }
 
   //validate password
-  if(empty(trim($_PSOT["password"]))){
-    $password_err = "Please Enter a Password"
+  if(empty(trim($_POST["password"]))){
+    $password_err = "Please Enter a Password";
   }elseif(strlen(trim($_POST['password']))<6) {
     $password_err = "Password must have at least 6 characters";
   }else{
@@ -58,14 +58,71 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
   //Check input errors before inserting anything into database
   if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-    //Prepare SQL statement
+    //Prepare SQL statement to insert
     $sql = "INSERT INTO users (username,password) VALUES (?,?)";
+    if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
 
-  }
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
 
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
 
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
 
-
-
+    // Close connection
+    mysqli_close($conn);
 }
- ?>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login2.php">Login here</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
