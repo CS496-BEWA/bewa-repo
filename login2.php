8835a06b256f438819cbf4fbbd458136eb582448
Zattack123 <zdilliha@gmail.com>
2021-03-17 12:55:03 -0500

    register now goes to correct place, and login2 has reset-password feature; still working on how to actually make it work

--- login2.php
+++ login2.php
@@ -118,6 +118,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+            <p>Forgot your password? <a href="reset-password.php">Reset Password</a>.</p>
         </form>
     </div>
 </body>
