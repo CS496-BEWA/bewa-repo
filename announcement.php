813e28a32465dd64f36f5408ea4a3ed111dbab48
EliSEstes <57155193+EliSEstes@users.noreply.github.com>
2021-04-10 10:29:04 -0500

    minor styling

--- announcement.php
+++ announcement.php
@@ -1,48 +1,30 @@
-<?php require 'includes/dbh.inc.php';
-session_start();?>
-
 <?php
+require 'header.php';
+?>
 
+<br><br><br>
 
+<div class="fs-3 mb-5 text-center">
+Post New Announcement
+</div>
 
-
- ?>
-
-
-
-
-
-
-
-
-
-<form name="form1" method="post" action="announcement.php">
-  <table width="50%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-      <td width="50%">Employee ID</td>
-      <td><input name="empID" type="text" id="empID" value=<?= $_SESSION['username']?>></td>
-    </tr>
-    <tr>
-      <td>Subject</td>
-      <td><input name="subject" type="text" id="subject" value=""></td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td>&nbsp;</td>
-    </tr>
-    <tr>
-      <td>Title</td>
-      <td><input name="title" type="text" id="title" value=""></td>
-    </tr>
-    <tr>
-      <td>Text</td>
-      <td><textarea name="text" id="text" value=""></textarea></td>
-    </tr>
-    <tr>
-      <td colspan="2"><div align="center">
-          <input name="hiddenField" type="hidden" value="update">
-          <input name="add" type="submit" id="add" value="Update">
-        </div></td>
-    </tr>
-  </table>
+  <form action="announcement.php" method="post" class="mx-auto" style="width: 400px;">
+            <div class="form-group mb-4">
+                <label class="form-label">Subject</label>
+                <input type="text" name="subject" id="subject" class="form-control" value="">
+                <span class="form-text"></span>
+            </div>
+            <div class="form-group mb-4">
+                <label>Title</label>
+                <input type="text" name="title" id="title" class="form-control" value="">
+                <span class="help-block"></span>
+            </div>
+            <div class="form-group mb-4">
+                <label>Text</label>
+                <input type="text" name="text" id="text" class="form-control" value="">
+                <span class="help-block"></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Post Announcement">
+            </div>
   </form>
