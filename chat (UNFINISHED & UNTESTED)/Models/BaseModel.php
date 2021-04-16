69d9c2818db73f773c2bffe258c7fcf9d4bd17c1
ClayGrant <56886897+ClayGrant@users.noreply.github.com>
2021-04-16 00:24:19 -0400

    Update BaseModel.php

--- chat (UNFINISHED & UNTESTED)/Models/BaseModel.php
+++ chat (UNFINISHED & UNTESTED)/Models/BaseModel.php
@@ -1,21 +1,11 @@
 <?php
 
-/**
- * This file is part of the TextmagicRestClient package.
- *
- * Copyright (c) 2015 TextMagic Ltd. All rights reserved.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
  
 namespace Textmagic\Services\Models; 
  
 use Textmagic\Services\TextmagicRestClient;
- 
- /**
- * @author Denis <denis@textmagic.biz>
- */
+
  
 class BaseModel {
 
